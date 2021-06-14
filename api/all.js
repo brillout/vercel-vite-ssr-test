5f023ce4c4a021501487eb433c6fd5a66835101f
mjarkk <mkopenga@gmail.com>
2021-06-14 22:42:25 +0200

    Reverting changes and add sugestion

---
+++ api/all.js
@@ -0,0 +1,15 @@
+import '../dist/server/importer.js';
+
+import { createPageRender } from 'vite-plugin-ssr'
+import { resolve } from 'path'
+
+const renderPage = createPageRender({ isProduction: true, root: resolve(__dirname, '..') });
+
+module.exports = async (req, res) => {
+    const result = await renderPage({
+        url: req.url,
+        pageProps: {},
+    });
+    if (result.nothingRendered) res.status(200).send('');
+    res.status(result.statusCode).send(result.renderResult);
+}
