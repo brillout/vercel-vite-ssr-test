ef0ae98317f60e6ea91dd639dc7dcfdb9eddbbe9
Romuald Brillout <git@brillout.com>
2021-07-14 18:26:52 +0200

    wip

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ var walk = function(dir) {
     });
     return results;
 }
-console.log(116)
+console.log(117)
 const files = walk(path.join(__dirname, '..')).filter(p => !p.includes('node_modules'))
 console.log(files)
 /*
