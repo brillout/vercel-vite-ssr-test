9a6cf0fe1ba727e7c9acf6fd76f7284624466cec
Romuald Brillout <git@brillout.com>
2021-07-14 19:27:02 +0200

    wip

--- gen.js
+++ gen.js
@@ -1,8 +1,10 @@
+console.log('aaa')
+/*
 const path = require('path');
 const fs = require('fs');
-console.log('bbbbbbb')
 
 const filePath = path.join(__dirname, 'public', 'geniFile.html')
 
 fs.mkdirSync(path.dirname(filePath))
 fs.writeFileSync(filePath, 'coniii')
+*/
