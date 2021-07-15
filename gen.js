d67d0b38e2108cb8310c716596ab6c10440cb557
Romuald Brillout <git@brillout.com>
2021-07-15 15:38:39 +0200

    Revert "wip"
    This reverts commit 30ffded6c125cf3b8892ca8721d7b60e85ccb123.

---
+++ gen.js
@@ -0,0 +1,10 @@
+console.log('aaa')
+/*
+const path = require('path');
+const fs = require('fs');
+
+const filePath = path.join(__dirname, 'public', 'geniFile.html')
+
+fs.mkdirSync(path.dirname(filePath))
+fs.writeFileSync(filePath, 'coniii')
+*/
